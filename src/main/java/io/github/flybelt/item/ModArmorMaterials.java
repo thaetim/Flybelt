294452fa3ab7362b4b283a8182c8ca3444cce6f6
Maciej Stawarz <maciey138@gmail.com>
2023-11-29 01:31:11 +0100

    init commit v0.0.4.0 items, recipes, armor, hunger

---
+++ src/main/java/io/github/flybelt/item/ModArmorMaterials.java
@@ -0,0 +1,79 @@
+package io.github.flybelt.item;
+
+import com.google.common.base.Supplier;
+
+import io.github.flybelt.FlybeltMod;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.item.ArmorItem.Type;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
+
+public enum ModArmorMaterials implements ArmorMaterial {
+    REDSTONE_ALLOY("redstone_alloy", 25, new int[] { 1, 3, 2, 1 }, 25, 
+        SoundEvents.ARMOR_EQUIP_ELYTRA, 0.25f, 0f, () -> Ingredient.of(Items.REDSTONE_BLOCK));
+
+    private final String name;
+    private final int durabilityMultiplier;
+    private final int[] protectionAmounts;
+    private final int enchantability;
+    private final SoundEvent equipSound;
+    private final float toughness;
+    private final float knockbackResistance;
+    private final Supplier<Ingredient> repairIngredient;
+
+    private static final int[] BASE_DURABILITY = { 5, 8, 7, 6 };
+
+    ModArmorMaterials(String name, int durabilityMultiplier, int[] protectionAmounts, int enchantability, SoundEvent equipSound, float toughness, float knockbackResistance, Supplier<Ingredient> repairIngredient) {
+        this.name = name;
+        this.durabilityMultiplier = durabilityMultiplier;
+        this.protectionAmounts = protectionAmounts;
+        this.enchantability = enchantability;
+        this.equipSound = equipSound;
+        this.toughness = toughness;
+        this.knockbackResistance = knockbackResistance;
+        this.repairIngredient = repairIngredient;
+    }
+
+    @Override
+    public int getDurabilityForType(Type type) {
+        return BASE_DURABILITY[type.ordinal()] * this.durabilityMultiplier;
+    }
+
+    @Override
+    public int getDefenseForType(Type type) {
+        return protectionAmounts[type.ordinal()];
+    }
+
+    @Override
+    public int getEnchantmentValue() {
+        return this.enchantability;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return this.equipSound;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return this.repairIngredient.get();
+    }
+
+    @Override
+    public String getName() {
+        return FlybeltMod.MODID + ":" + this.name;
+    }
+
+    @Override
+    public float getToughness() {
+        return this.toughness;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return this.knockbackResistance;
+    }
+    
+}
---
+++ src/main/java/io/github/flybelt/item/ModCreativeModTabs.java
@@ -0,0 +1,31 @@
+package io.github.flybelt.item;
+
+import io.github.flybelt.FlybeltMod;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModCreativeModTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = 
+        DeferredRegister.create(Registries.CREATIVE_MODE_TAB, FlybeltMod.MODID);
+
+    public static final RegistryObject<CreativeModeTab> FLYBELT_TAB = CREATIVE_MODE_TABS.register("flybelt",
+        () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.REDSTONE_ALLOY_INGOT.get()))
+        .title(Component.translatable("creativetab.flybelt_tab"))
+        .displayItems((pParameters, pOutput) -> {
+            // the order of items here is the order in the tab
+            pOutput.accept(ModItems.RAW_REDSTONE_ALLOY.get());
+            pOutput.accept(ModItems.REDSTONE_ALLOY_INGOT.get());
+            pOutput.accept(ModItems.FLYBELT.get());
+        })
+        .build());
+    
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+    
+}
